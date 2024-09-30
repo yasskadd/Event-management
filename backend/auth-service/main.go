1a1a53f9f6e4c34a31558e866d272f733222adfd
Yasser <yasser.kadimi@gmail.com>
2024-09-30 16:33:35 -0500

    Added connection to postgres db in auth-service

--- backend/auth-service/main.go
+++ backend/auth-service/main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq" //PostgreSQL Driver
 	"github.com/yasskadd/Event-management/auth-service/routes"
 )
 
 func main() {
+
+	host := "postgres"
+	port := 5432
+	user := os.Getenv("DB_USER")
+	dbname := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASSWORD")
+
+	connectionStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// Connect to the DB
+	db, err := sql.Open("postgres", connectionStr)
+
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v\n", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Could not ping the database: %v\n", err)
+	}
+
 	router := gin.Default()
 
 	// Setup routes
