3de15988b8bc9c6d01f206c85cdfe296617d25b8
yasskadd <71228349+yasskadd@users.noreply.github.com>
2024-10-01 22:28:19 -0500

    added username and email regex verification

--- backend/auth-service/models/user.go
+++ backend/auth-service/models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"regexp"
 	"time"
 )
 
@@ -14,5 +15,26 @@ type User struct {
 }
 
 func RegisterUser(db *sql.DB, username string, email string, password string) error {
+	// Validate the username and password with Regex
+
+	// Check if email is already taken
+
+	//Check if username is already taken
+
+	// If everythings good, hash the password and add user to DB
 	return nil
 }
+
+func IsUsernameValid(username string) bool {
+	reg := regexp.MustCompile(`^[a-zA-Z0-9]{5,20}$`)
+	return reg.MatchString(username)
+}
+
+func isEmailValid(email string) bool {
+	reg := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return reg.MatchString(email)
+}
+
+func IsUsernameTaken()
+
+func IsEmailTaken()
