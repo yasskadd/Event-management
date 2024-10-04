fe414235f24eaa3129099367e11977d06c25435a
yasskadd <71228349+yasskadd@users.noreply.github.com>
2024-10-04 02:55:33 -0500

    Implemented Register handler and error handling

--- backend/auth-service/handlers/handlers.go
+++ backend/auth-service/handlers/handlers.go
@@ -1,15 +1,48 @@
 package handlers
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yasskadd/Event-management/auth-service/models"
 )
+
+type RegistrationRequest struct {
+	Username string `json:"username"` // Tags can be used to specify how to marshal/unmarshal JSON
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 // Register handles user registration
 func Register(c *gin.Context) {
-	// Logic for user registration
-	c.JSON(http.StatusOK, gin.H{"message": "User registered successfully!"})
+	var request RegistrationRequest
+
+	// Bind the JSON input to the request struct
+	if err := c.BindJSON(&request); err != nil {
+		fmt.Println("Error binding JSON:", err)
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	// Get the DB connection from the context (passed from middleware)
+	var db *sql.DB = c.MustGet("db").(*sql.DB)
+	fmt.Println("Database connection established.")
+
+	// Try to register the user
+	success, errors := models.RegisterUser(db, request.Username, request.Email, request.Password)
+
+	if !success {
+		fmt.Println("User registration failed:", errors)
+		var errorMessages []string
+		for _, err := range errors {
+			errorMessages = append(errorMessages, err.Error())
+		}
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"errors": errorMessages})
+		return
+	}
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": "User registered successfully!"})
 }
 
 // Login handles user login
