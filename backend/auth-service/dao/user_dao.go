b24de83aa2ac8420d9b194c57f38be2cf313db5d
yasskadd <71228349+yasskadd@users.noreply.github.com>
2024-10-04 10:13:00 -0500

    New dao file to handle user db request

---
+++ backend/auth-service/dao/user_dao.go
@@ -0,0 +1,52 @@
+package dao
+
+import (
+	"database/sql"
+)
+
+type User struct {
+	ID       int64
+	Username string
+	Password string
+}
+
+// GetUser retrieves a user by their ID.
+func GetUserById(db *sql.DB, userID int64) (*User, error) {
+	var user User
+	query := "SELECT id, username, password FROM users WHERE id = ?"
+
+	err := db.QueryRow(query, userID).Scan(&user.ID, &user.Username, &user.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil // No user found
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func GetUserByEmail(db *sql.DB, email string) (*User, error) {
+	var user User
+	query := "SELECT id, username, password FROM users WHERE username = ?"
+
+	err := db.QueryRow(query, email).Scan(&user.ID, &user.Username, &user.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil // No user found
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+// UserExists checks if a user exists by their username.
+func UserExists(db *sql.DB, username string) (bool, error) {
+	var exists bool
+	query := "SELECT EXISTS(SELECT 1 FROM users WHERE username = ?)"
+
+	err := db.QueryRow(query, username).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
+}
