acecbd001385a27884c645c754de6e41bde2ff53
yasskadd <71228349+yasskadd@users.noreply.github.com>
2024-10-04 00:46:54 -0500

    Added RegistrationRequest struct

--- backend/auth-service/routes/routes.go
+++ backend/auth-service/routes/routes.go
@@ -5,6 +5,12 @@ import (
 	"github.com/yasskadd/Event-management/auth-service/handlers"
 )
 
+type RegistrationRequest struct {
+	Username string `json:"username"` // Tags can be used to specify how to marshal/unmarshal JSON
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 // SetupRoutes initializes the routes for the auth service
 func SetupRoutes(router *gin.Engine) {
 	router.POST("/register", handlers.Register) // Placeholder for registration handler
