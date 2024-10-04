f919481709991d921c89b7a0e3ce28ef711f6a91
yasskadd <71228349+yasskadd@users.noreply.github.com>
2024-10-04 04:33:51 -0500

    Implemented JWT token generation with expiration set to one month, including userID and username in claims.

--- backend/auth-service/models/auth.go
+++ backend/auth-service/models/auth.go
@@ -2,6 +2,28 @@ package models
 
 import (
 	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 var secretKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+
+const MONTH_TO_HOURS int = 730
+
+func GenerateToken(userID int64, username string) (string, error) {
+	token := jwt.New(jwt.SigningMethodES256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["userID"] = userID                                                        // Set the userID in payload
+	claims["username"] = username                                                    // Set the username in payload
+	claims["exp"] = time.Now().Add(time.Hour * time.Duration(MONTH_TO_HOURS)).Unix() //Set token expiration to one month
+
+	//Sign the token with the secret key
+	tokenStr, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenStr, nil
+}
