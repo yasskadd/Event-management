1bd399146a7460c4a99431e60c6256e6ba44ff49
yasskadd <71228349+yasskadd@users.noreply.github.com>
2024-11-16 14:07:05 -0500

    Initial event-service setup

---
+++ backend/event-service/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"event-service/routes"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Set up the routes
+	routes.SetupRoutes(router)
+
+	// Start the server on port 8080
+	log.Println("Starting event-service on port 8080")
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+	log.Println("Server succesfully started")
+}
