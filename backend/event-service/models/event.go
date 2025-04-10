3957eaaf22712f584e423211f8c7c08404309133
yasskadd <71228349+yasskadd@users.noreply.github.com>
2025-04-10 18:03:07 -0400

    modified Event struct

--- backend/event-service/models/event.go
+++ backend/event-service/models/event.go
@@ -8,7 +8,10 @@ type Event struct {
 	Description string    `json:"description"`
 	Location    string    `json:"location"`
 	Date        time.Time `json:"date"`
-	OrganizerID uint      `json:"organizer_id"`
+	Status      string    `json:"status"`       // "online", "offline", "waitlist"
+	TicketCount int       `json:"ticket_count"` // Total tickets available
+	TicketsSold int       `json:"tickets_sold"` // Tickets sold
+	OrganizerID uint      `json:"organizer_id"` // FK to User
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
